8a26339168ad5b2083cdeff47a681a0ebf8da96f
yuranimar <78806776+yuranimar@users.noreply.github.com>
2026-05-07 21:57:31 -0500

    Add base path for stockflow-react

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: '/stockflow-react/', // <--- AGREGA ESTA LÍNEA EXACTAMENTE ASÍ
 })
